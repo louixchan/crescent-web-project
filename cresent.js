244de51868d33a6551695ff5175f9a1f6f4a0496
louixchan <louixchan.cly@gmail.com>
2017-06-29 00:16:25 +0100

    edited responsive top carousel and overlay

---
+++ cresent.js
@@ -0,0 +1,39 @@
+onDomReady( function() {
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $document.getElementByClassName('header').removeClass('nav-down').addClass('nav-up');
+            console.log("Test down");
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $document.getElementByClassName('header').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+    }
+});
